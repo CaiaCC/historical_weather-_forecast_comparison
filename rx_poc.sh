59a10209de7a6030dc756732a3ba68413ace87ef
Caia Chuang <caiachuang@gmail.com>
2024-02-14 16:32:47 -0800

    extracted forcast temperature for tomorrow noon, and stored it in a shell var

--- rx_poc.sh
+++ rx_poc.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
-#To extract Current Temperature
+# Extract Current Temperature
 curr_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
 echo "The current Temperature of $city: $curr_temp"
+
+# Extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
