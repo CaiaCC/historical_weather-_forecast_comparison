e3311e5e0c7c9092e9598ec6c55ac5070ed17c2e
Caia Chuang <caiachuang@gmail.com>
2024-02-14 16:31:39 -0800

    extracted current temperature, and stored it in a shell var

--- rx_poc.sh
+++ rx_poc.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+
+#To extract Current Temperature
+curr_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $curr_temp"
