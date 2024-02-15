32295db2526cc5ff8e8b4f4430d2133effdb5ed8
Caia Chuang <caiachuang@gmail.com>
2024-02-14 16:35:42 -0800

    set timezone, and store current day, month and year in shell variables

--- rx_poc.sh
+++ rx_poc.sh
@@ -7,3 +7,12 @@ echo "The current Temperature of $city: $curr_temp"
 # Extract the forecast tempearature for noon tomorrow
 fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
 echo "The forecasted temperature for noon tomorrow for $city : $fc_temp C"
+
+# Assign Country and City to variable TZ
+TZ='Canada/Vancouver'
+
+# Use command substitution to store the current day, month, and year in corresponding shell variables
+hour=$(TZ='Canada/Vancouver' date -u +%H)
+day=$(TZ='Canada/Vancouver' date -u +%d)
+month=$(TZ='Canada/Vancouver' date +%m)
+year=$(TZ='Canada/Vancouver' date +%Y)
